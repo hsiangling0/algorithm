6fa879a8bf8b43ca6016537503794182492439a7
hsiangling0 <97903196+hsiangling0@users.noreply.github.com>
2022-03-20 21:13:43 +0800

    Add files via upload

---
+++ hw2-2-2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void print(bool record[],vector<int> num,int amount){
+	for(int i=0;i<amount;i++){
+		if(record[i]){
+			cout<<num[i]<<" ";
+		}
+	}
+	cout<<endl;
+}
+
+bool check(bool record[],int amount){
+	for(int i=amount-6;i<amount;i++){
+		if(!record[i]){
+			return false;
+		}
+	}
+	return true;
+}
+void com(vector<int> &num,int amount){
+	bool *record=new bool[amount]();
+	for(int i=0;i<amount;i++){
+		if(i>5){
+			record[i]=false;
+		}
+		else{
+			record[i]=true;
+		}
+	}
+	int rot=5;
+	print(record,num,amount);
+	while(!check(record,amount)){
+		for(int i=amount-1;i>0;i--){
+			if(!record[i]&&record[i-1]){
+				record[i]=true;
+				record[i-1]=false;
+				int temp=i+1;
+				for(int j=i+1;j<amount;j++){
+					if(record[j]){
+						record[j]=false;
+						record[temp++]=true;
+					}
+				}
+				print(record,num,amount);
+				break;
+			}	
+		}
+	}
+	delete[] record;
+}
+main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int amount,fix;
+	cin>>amount;
+	vector<int> num;
+	int pick;
+	for(int i=0;i<amount;i++){
+		cin>>pick;
+		num.push_back(pick);
+	}
+	
+	com(num,amount);
+}
