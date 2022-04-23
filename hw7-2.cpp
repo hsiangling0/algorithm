e470ae62c346ca050c0ba6180e3f9b23be071a5e
hsiangling0 <97903196+hsiangling0@users.noreply.github.com>
2022-04-23 12:46:41 +0800

    Add files via upload

---
+++ hw7-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findMax(vector<int> &value,vector<int> &cost,int amount, int timeLimit){
+	int ans[2][timeLimit+1];
+	for(int i=0;i<=amount;i++){
+		for(int j=0;j<=timeLimit;j++){
+			if(i==0||j==0){
+				ans[i%2][j]=0;
+			}
+			else if(cost[i]>j)ans[i%2][j]=ans[(i-1)%2][j];
+			else{
+				ans[i%2][j]=max(ans[(i-1)%2][j],value[i]+ans[(i-1)%2][j-cost[i]]);
+			}
+		}
+	}
+	cout<<ans[amount%2][timeLimit]<<endl;
+}
+
+main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int amount,timeLimit,value,cost;
+	cin>>amount>>timeLimit;
+	vector<int> Val;
+	vector<int> Cost;
+	Val.push_back(0);
+	Cost.push_back(0);
+	while(cin>>value>>cost){
+		Val.push_back(value);
+		Cost.push_back(cost);
+	}
+	findMax(Val,Cost,amount,timeLimit);
+}
